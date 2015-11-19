9d526088761041a42f28225b04806922f2cf1f85
kim KB <win6656@nate.com>
2015-11-19 18:56:23 +0900

    AOP update

--- src/main/java/com/hybrid/service/DeptRegisterService.java
+++ src/main/java/com/hybrid/service/DeptRegisterService.java
@@ -1,5 +1,7 @@
 package com.hybrid.service;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -8,14 +10,21 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import com.hybrid.dao.DeptDao;
+import com.hybrid.dao.EmpDao;
 import com.hybrid.model.Dept;
+import com.hybrid.model.Emp;
 
 public class DeptRegisterService {
 
 	DeptDao deptDao;
+	EmpDao empDao;
 	
 	public void setDeptDao(DeptDao dao) {
 		this.deptDao = dao;
+	}
+	
+	public void setEmpDao(EmpDao dao) {
+		this.empDao = dao;
 	}
 	
 	DataSource dataSource;
@@ -26,19 +35,13 @@ public class DeptRegisterService {
 		
 	public void regist(Dept dept) {
 		
-		DataSourceTransactionManager transactionManager=null;
-		transactionManager = new DataSourceTransactionManager();
-		transactionManager.setDataSource(dataSource);
+		deptDao.insert(dept);
 		
-		TransactionDefinition td = new DefaultTransactionDefinition();
-		TransactionStatus ts = transactionManager.getTransaction(td);
-		
-		try {
-			deptDao.insert(dept);
-			transactionManager.commit(ts);
-		} catch (RuntimeException ex) {
-			ex.printStackTrace();
-			transactionManager.rollback(ts);	
+		List<Emp> emps = dept.getEmps();
+
+		if (emps != null)
+		for (Emp e : emps) {
+			empDao.insert(e);
 		}
 		
 	}
